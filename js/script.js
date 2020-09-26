f694a9133e842f171cf0011331e3481f215e9b47
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-09-26 15:57:04 -0400

    complete quotes array

--- js/script.js
+++ js/script.js
@@ -4,20 +4,47 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * `quotes` array 
 ***/
 
-
+const quotes = [
+  {
+    quote: "The unexamined life is not worth living.",
+    source: "Socrates",
+    citation: "Apology of Socrates",
+    year: "399 BCE"
+  },
+  {
+    quote: "I love being a carbon molecule.",
+    source: "Duncan Trussell"
+  },
+  {
+    quote: "If you tried to give rock and roll another name, you might call it Chuck Berry.",
+    source: "Keith Richards",
+    citation: "The Mike Douglas Show",
+    year: 1972
+  },
+  {
+    quote: "You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.",
+    source: "Dr. Suess"
+  },
+  {
+    quote: "Education is the passport to the future, for tomorrow belongs to those who prepare for it today.",
+    source: "Malcolm X"
+  }
+];
 
 /***
  * `getRandomQuote` function
 ***/
 
+const getRandomQuote = () => {
 
+}
 
 /***
  * `printQuote` function
